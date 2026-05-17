6108ca77d932482919deb695fdaba06116e4e27b
gangsarrz <gangsarrizkiyansah80@gmail.com>
2026-05-17 15:50:36 +0700

    first commit

---
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/Main.java
@@ -0,0 +1,17 @@
+package com.dicoding.javafundamental.basic;
+
+import com.dicoding.javafundamental.basic.kendaraan.*;
+import com.dicoding.javafundamental.basic.musik.Gitar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        Gitar.bunyi();
+
+        Mobil.tampilkanJumlahBan();
+        Motor.tampilkanJumlahBan();
+        Kereta.tampilkanJumlahBan();
+
+    }
+}
---
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/Main1.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.basic;
+
+public class Main1 {
+    public static void main(String[] args) {
+        boolean menyala = false;
+
+        if(menyala){
+            System.out.println("Menyalakan lampu");
+        }else {
+            System.out.println("Kondisi tidak terpenuhi");
+        }
+    }
+}
